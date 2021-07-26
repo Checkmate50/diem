b9518a084618120fb3717dcaacd6d4d98b7e68e1
checkmate50 <dag368@cornell.edu>
2021-07-26 16:51:17 -0600

    Baseline mutator put together

--- language/move-prover/mutation/src/mutator.rs
+++ language/move-prover/mutation/src/mutator.rs
@@ -70,48 +70,28 @@ pub fn mutate(args: &[String]) {
     let sources = get_vec("sources");
     let deps = get_vec("dependencies");
 
-    for config_spec in configs {
-        let (config, out) = if let Some(config_file) = &config_spec {
-            let extension = if per_function { "fun_data" } else { "mod_data" };
-            let out = PathBuf::from(config_file)
-                .with_extension(extension)
-                .to_string_lossy()
-                .to_string();
-            (config_spec, out)
-        } else {
-            (None, "benchmark.data".to_string())
-        };
-        if let Err(s) = run_benchmark(&out, config.as_ref(), &sources, &deps, per_function) {
-            println!("ERROR: execution failed: {}", s);
-        } else {
-            println!("results stored at `{}`", out);
-        }
+    if let Err(s) = apply_mutation(&out, &sources, &deps) {
+        println!("ERROR: execution failed: {}", s);
+    } else {
+        println!("results stored at `{}`", out);
     }
 }
 
 fn apply_mutation(
     out: &str,
-    config_file_opt: Option<&String>,
     modules: &[String],
     dep_dirs: &[String],
-    per_function: bool,
 ) -> anyhow::Result<()> {
     println!("building model");
     let env = run_model_builder(modules, dep_dirs)?;
     let mut error_writer = StandardStream::stderr(ColorChoice::Auto);
-    let mut options = if let Some(config_file) = config_file_opt {
-        Options::create_from_toml_file(config_file)?
-    } else {
-        Options::default()
-    };
+    let mut options = Options::default();
 
-    // Do not allow any benchmark to run longer than 100s. If this is exceeded it usually
-    // indicates a bug in boogie or the solver, because we already propagate soft timeouts, but
-    // they are ignored.
+    // Do not allow any mutation to run longer than 100 seconds to avoid absolute insanity
     options.backend.hard_timeout_secs = 100;
 
-    options.verbosity_level = LevelFilter::Warn;
-    options.backend.proc_cores = 1;
+    options.verbosity_level = LevelFilter::Error;
+
     options.backend.derive_options();
     options.setup_logging();
     check_errors(&env, &options, &mut error_writer, "unexpected build errors")?;
@@ -121,11 +101,6 @@ fn apply_mutation(
     } else {
         "default".to_string()
     };
-
-    let mut out = LineWriter::new(File::create(out)?);
-
-    writeln!(out, "# config: {}", config_descr)?;
-    writeln!(out, "# time  : {}", chrono::Utc::now())?;
 
     let mut runner = Runner {
         options,
@@ -138,51 +113,21 @@ fn apply_mutation(
         Notice that execution is slow because we enforce single core execution.",
         config_descr
     );
-    runner.bench(&env)
+    runner.mutate(&env)
 }
 
 impl Runner {
-    fn bench(&mut self, env: &GlobalEnv) -> anyhow::Result<()> {
+    fn mutate(&mut self, env: &GlobalEnv) -> anyhow::Result<()> {
         for module in env.get_modules() {
             if module.is_target() {
-                if self.per_function {
-                    for fun in module.get_functions() {
-                        self.bench_function(fun)?;
-                    }
-                } else {
-                    self.bench_module(module)?;
-                }
+                self.mutate_module(module);
             }
         }
         Ok(())
     }
 
-    fn bench_function(&mut self, fun: FunctionEnv<'_>) -> anyhow::Result<()> {
-        print!("benchmarking function {} ..", fun.get_full_name_str());
-        std::io::stdout().flush()?;
-
-        // Scope verification to the given function
-        let env = fun.module_env.env;
-        self.options.prover.verify_scope = VerificationScope::Only(fun.get_full_name_str());
-        ProverOptions::set(env, self.options.prover.clone());
-        // Run benchmark
-        let (duration, status) = self.bench_function_or_module(fun.module_env.env)?;
-
-        // Write data record of benchmark result
-        writeln!(
-            self.out,
-            "{:<40} {:>12} {:>12}",
-            fun.get_full_name_str(),
-            duration.as_millis(),
-            status
-        )?;
-
-        println!("\x08\x08{:.3}s {}.", duration.as_secs_f64(), status);
-        Ok(())
-    }
-
-    fn bench_module(&mut self, module: ModuleEnv<'_>) -> anyhow::Result<()> {
-        print!("benchmarking module {} ..", module.get_full_name_str());
+    fn mutate_module(&mut self, module: ModuleEnv<'_>) -> anyhow::Result<()> {
+        print!("mutating module {} ..", module.get_full_name_str());
         std::io::stdout().flush()?;
 
         // Scope verification to the given module
@@ -191,7 +136,7 @@ impl Runner {
         ProverOptions::set(module.env, self.options.prover.clone());
 
         // Run benchmark
-        let (duration, status) = self.bench_function_or_module(module.env)?;
+        let (duration, status) = self.mutate_module_duration(module.env)?;
 
         // Write data record of benchmark result
         writeln!(
@@ -206,7 +151,7 @@ impl Runner {
         Ok(())
     }
 
-    fn bench_function_or_module(&mut self, env: &GlobalEnv) -> anyhow::Result<(Duration, String)> {
+    fn mutate_module_duration(&mut self, env: &GlobalEnv) -> anyhow::Result<(Duration, String)> {
         // Create and process bytecode.
         let targets = create_and_process_bytecode(&self.options, env);
         check_errors(
@@ -242,102 +187,4 @@ impl Runner {
         env.clear_diag();
         Ok((now.elapsed(), status.to_string()))
     }
-}
-
-// ============================================================================================
-// Reading and manipulating benchmark data
-
-/// Represents a benchmark.
-#[derive(Clone, Debug)]
-pub struct Benchmark {
-    /// The simple name of the configuration.
-    pub config: String,
-    /// The associated data.
-    pub data: Vec<BenchmarkData>,
-}
-
-/// A data entry of a benchmark.
-#[derive(Clone, Debug)]
-pub struct BenchmarkData {
-    pub name: String,
-    pub duration: usize,
-    pub status: String,
-}
-
-/// Read benchmark from data file.
-pub fn read_benchmark(data_file: &str) -> anyhow::Result<Benchmark> {
-    let config = PathBuf::from(data_file)
-        .with_extension("") // remove extension
-        .file_name() // use simple filename
-        .ok_or_else(|| anyhow!("invalid data file name"))?
-        .to_string_lossy()
-        .to_string();
-    let content = std::fs::read_to_string(data_file)?;
-    let mut data = vec![];
-    for line in content.lines() {
-        if line.starts_with('#') {
-            continue;
-        }
-        let parts = line.split_whitespace().collect_vec();
-        if parts.len() != 3 {
-            return Err(anyhow!("bad data entry"));
-        }
-        let name = parts[0].to_string();
-        let duration = parts[1].parse::<usize>()?;
-        let status = parts[2].to_string();
-        data.push(BenchmarkData {
-            name,
-            duration,
-            status,
-        });
-    }
-    Ok(Benchmark { config, data })
-}
-
-impl Benchmark {
-    /// Sort the benchmark data by longest duration.
-    pub fn sort(&mut self) {
-        self.data
-            .sort_by(|d1, d2| d1.duration.cmp(&d2.duration).reverse());
-    }
-
-    /// Shrink benchmark to the first count samples.
-    pub fn take(&mut self, count: usize) {
-        self.data.truncate(count)
-    }
-
-    /// Sum the durations of the samples in the benchmark.
-    pub fn sum(&self) -> u32 {
-        self.data
-            .iter()
-            .filter_map(|d| {
-                if d.status == "ok" || d.status == "error" {
-                    Some(d.duration as u32)
-                } else {
-                    None
-                }
-            })
-            .sum()
-    }
-}
-
-/// Print statistics for the given set of benchmarks.
-/// TODO: would be nice to have a histogram instead of textual output.
-pub fn stats_benchmarks(benchmarks: &[&Benchmark]) -> String {
-    let baseline = benchmarks[0].sum() as f32 / 1000.0;
-    let mut res = String::new();
-    let config_width = benchmarks.iter().map(|b| b.config.len()).max().unwrap();
-    for benchmark in benchmarks {
-        let sum = benchmark.sum() as f32 / 1000.0;
-        let factor = sum / baseline;
-        res = format!(
-            "{}\n{:width$}: {:.3}s tot, {:.3} rel",
-            res,
-            benchmark.config,
-            sum,
-            factor,
-            width = config_width,
-        );
-    }
-    res
 }
